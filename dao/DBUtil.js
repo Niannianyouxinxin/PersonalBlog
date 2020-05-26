edd7cae24c1f02384ba94ab870166e89d3183d71
Niannianyouxinxin <3073007696@qq.com>
2020-05-26 21:26:56 +0800

    first

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 function createConnection() {
     var connection = mysql.createConnection({
-        host:"localhost",
+        host:"172.26.194.112",
         port:"3306",
         user:"root",
-        password:"123456",
+        password:"Xin19990611;",
         database:"my_blog"
     });
     return connection;
